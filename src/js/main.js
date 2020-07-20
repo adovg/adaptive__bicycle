12b374c7ba0b9a564abfd0d7e2e74a6dbcd4332c
adovg <you@example.com>
2020-07-21 00:12:13 +0300

    shop & btn

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,32 @@
-function test() {
-	alert('hello js');
-	let sum = (firstNum + secondNum);
-	console.console.log(sum);
-};
+// function test() {
+// 	alert('hello js');
+// 	let sum = (firstNum + secondNum);
+// 	console.console.log(sum);
+// };
 
 
 
-let firstNum = 5,
-	secondNum = 10;
+// let firstNum = 5,
+// 	secondNum = 10;
 
-test();
+// test();
+
+// let getArticle = () => {
+// 	//console.log('!!!');
+// 	let article = document.querySelector('.shop__article');
+// 	//console.log(article);
+// 	 return article.style.visibility = "visible";
+// 	// if (article.style.visibility = "hidden") {
+// 	// 	console.log(article);
+// 	// 	article.style.visibility = "visible";
+// 	// }else if (article.style.visibility = "visible"){
+// 	// 	article.style.visibility = "hidden";
+// 	// }
+// };
+
+// //ul.innerHTML += "<li>1</li><li>2</li>...";
+
+// let ereseArticle = () => {
+// 	let article = document.querySelector('.shop__article');
+// 	return article.style.visibility = "hidden";
+// }
